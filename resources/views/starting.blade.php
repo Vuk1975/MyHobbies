df97c49460a58fd8a1faaa185a2b776037a43c8a
vukojevic1975 <vukojevic1975@gmail.com>
2020-07-05 14:22:46 +0200

    Ispravljeni linkovi, scss Migracija tabele za log in Title, Description

---
+++ resources/views/starting.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Strating page</div>
+
+                <div class="card-body">
+                NEsto Nesto nesto
+                NEsto Nesto nesto
+                NEsto Nesto nesto
+                NEsto Nesto nesto
+                NEsto Nesto nesto
+                NEsto Nesto nesto NEsto Nesto nesto
+                NEsto Nesto nesto
+               
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
